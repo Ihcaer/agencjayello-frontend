4a8bd330503de48e68825eda9e339b926e54a2eb
Ihcaer <jakub2002410@wp.pl>
2025-04-21 16:36:37 +0200

    refactor: add lazy loading of home view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from '@main/views/main/home/home.component';
 import { MainComponent } from '@main/views/main/main.component';
 
 export const routes: Routes = [
    {
       path: '', component: MainComponent, children: [
-         { path: '', component: HomeComponent }
+         {
+            path: '',
+            loadComponent: () => import('./main/views/main/home/home.component').then(m => m.HomeComponent)
+         }
       ]
    }
 ];
